b195f83b7b81cc2d42af8dc7c396fbce4465a2ad
wylchayer <wyldacruz@gmail.com>
2023-03-09 14:58:15 -0300

    Resolução projeto BlackJack

--- nossoJogo.js
+++ nossoJogo.js
@@ -10,3 +10,38 @@
  * 
  * 
  */
+
+alert(`Boas vindas ao jogo de BlackJack!`)
+
+const iniciarRodada = confirm(`Quer iniciar uma nova rodada?`)
+const cartasUsuario = []
+const cartasComputador = []
+
+let pontuacaoUsuario, pontuacaoComputador
+
+
+if (iniciarRodada === true) {
+
+   cartasUsuario[0] = comprarCarta()
+   cartasUsuario[1] = comprarCarta()
+   pontuacaoUsuario = cartasUsuario[0].valor + cartasUsuario[1].valor
+
+   cartasComputador[0] = comprarCarta()
+   cartasComputador[1] = comprarCarta()
+   pontuacaoComputador = cartasComputador[0].valor + cartasComputador[1].valor
+
+   const mensagemCartas =`Usuário - cartas: ${cartasUsuario[0].texto} ${cartasUsuario[1].texto} - pontuação ${pontuacaoUsuario}
+Computador - cartas: ${cartasComputador[0].texto} ${cartasComputador[1].texto} - pontuação ${pontuacaoComputador}.`
+   
+   // console.log(`Usuário - cartas: ${cartasUsuario[0].texto} ${cartasUsuario[1].texto} - pontuação ${pontuacaoUsuario}`)
+   // console.log(`Computador - cartas: ${cartasComputador[0].texto} ${cartasComputador[1].texto} - pontuação ${pontuacaoComputador}`)
+
+   pontuacaoUsuario === pontuacaoComputador && pontuacaoUsuario === 22 ? alert(mensagemCartas + '\nDerrota empatada. Ambos estouraram') : 
+   pontuacaoUsuario > 21 ? alert(mensagemCartas + '\nUsuário estourou. O computador ganhou!') :
+   pontuacaoComputador > 21 ? alert(mensagemCartas + '\nComputador estourou. O usuário ganhou!') :
+   pontuacaoUsuario === pontuacaoComputador ? alert(mensagemCartas + '\nEmpate!') :
+   pontuacaoUsuario > pontuacaoComputador ? alert(mensagemCartas + '\nO usuário ganhou!') : alert(mensagemCartas + '\nO computador ganhou!')
+
+} else {
+   alert(`O jogo acabou!`)
+}
